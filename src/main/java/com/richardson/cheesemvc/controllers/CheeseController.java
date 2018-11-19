f989321b0b1c67c3536af66b6a8a1dd3aa5a4052
CordyRichardson <cordyrichardson3@gmail.com>
2018-11-19 00:05:29 -0500

    class 4 prep video2: added "add cheese form" to java added "add.html" file added PUT route handler to add route that redirects back to index

--- src/main/java/com/richardson/cheesemvc/controllers/CheeseController.java
+++ src/main/java/com/richardson/cheesemvc/controllers/CheeseController.java
@@ -3,7 +3,12 @@ package com.richardson.cheesemvc.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
 
 @Controller
 @RequestMapping("cheese")
@@ -15,11 +20,28 @@ public class CheeseController {
     index: ""
      */
 
+    ArrayList<String> cheeses = new ArrayList<>();
+
     @RequestMapping(value="")
     public String index(Model model){
         model.addAttribute("title", "My Cheeses");
+        model.addAttribute("cheeses", cheeses);
         return "cheese/index";
     }
+
+    @RequestMapping(value="add", method=RequestMethod.GET)
+    public String displayAddCheeseForm(Model model){
+        model.addAttribute("title", "Add Cheese");
+        return "cheese/add";
+    }
+
+    @RequestMapping(value="add", method= RequestMethod.POST)
+    public String processAddCheeseForm(@RequestParam String cheesename) {
+        cheeses.add(cheesename);
+        return "redirect:";
+
+    }
+
 
 
 }
