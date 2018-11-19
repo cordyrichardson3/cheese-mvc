636b9309a78a1def1fff8b1e1915c548e58c59b2
CordyRichardson <cordyrichardson3@gmail.com>
2018-11-18 22:18:05 -0500

    gradle wrapper update; init controller

---
+++ src/main/java/com/richardson/cheesemvc/controllers/CheeseController.java
@@ -0,0 +1,16 @@
+package com.richardson.cheesemvc.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("cheese")
+public class CheeseController {
+    /*
+    route definitions:
+    add: cheese/add
+    remove: cheese/remove
+    index: ""
+
+     */
+}
